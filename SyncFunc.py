1a9d3c922fd86fa8940fd19b2ada25adb76a8a28
Joshua Pu <death_finger@sina.com>
2016-06-27 12:19:46 +0800

    update from company

--- SyncFunc.py
+++ SyncFunc.py
@@ -60,7 +60,7 @@ def compare_new(pth_src, pth_des, lbl_list):
     return sync_list, file_exist
 
 
-def compare_exist(file_exist, pth_src, pth_des, changed_list, lbl):
+def compare_exist(file_exist, pth_src, pth_des, changed_list, lbl, btn):
     count = count_diff = 0
     for item in file_exist:
         pth_from = os.path.join(pth_src, item)
@@ -82,66 +82,8 @@ def compare_exist(file_exist, pth_src, pth_des, changed_list, lbl):
                 break
         count += 1
         lbl.config(text='%s/%s' % (count_diff, count))
+    btn.config(state=ACTIVE)
 
-"""
-def compare(pth_src, pth_des, lbl_list):
-    file_new = []
-    dir_new = []
-    file_exist = []
-
-    if not (pth_src and os.path.exists(pth_src)):
-        print('Source Path Not exist!')
-        return
-    elif not (pth_des and os.path.exists(pth_des)):
-        print('Destination Path Not Exist!')
-        if askyesno('Create Destination Path [%s]?' % pth_des):
-            os.mkdir(pth_des)
-        else:
-            return
-
-    src_pth = pth_src if pth_src[-1] == '/' else pth_src + '/'
-    des_pth = pth_des if pth_des[-1] == '/' else pth_des + '/'
-    src_pth_list = [x.replace(src_pth, '') for x in listdir(src_pth)]
-    des_pth_list = [x.replace(des_pth, '') for x in listdir(des_pth)]
-
-    lbl_list['Total'].config(text=len(src_pth_list))
-
-    for item in src_pth_list:
-        if item not in des_pth_list:
-            if os.path.isdir(os.path.join(src_pth, item)):
-                dir_new.append(item)
-            else:
-                file_new.append(item)
-        else:
-            file_exist.append(item)
-
-        lbl_list['NewFiles'].config(text=len(file_new))
-        lbl_list['NewFolders'].config(text=len(dir_new))
-
-    changed_list = []
-    for item in file_exist:
-        pth_from = os.path.join(pth_src, item)
-        pth_to = os.path.join(pth_des, item)
-        if os.path.isdir(pth_from): continue
-        src_file = open(pth_from, 'rb')
-        des_file = open(pth_to, 'rb')
-        src_hash = hashlib.sha1()
-        des_hash = hashlib.sha1()
-        while True:
-            src_data = src_file.read(20000000)
-            des_data = des_file.read(20000000)
-            if not (src_data and des_data): break
-            src_hash.update(src_data)
-            des_hash.update(des_data)
-            if src_hash.hexdigest() != des_hash.hexdigest():
-                changed_list.append(item)
-                break
-
-        lbl_list['Diff'].config(text=len(changed_list))
-
-    sync_list = (dir_new, file_new, changed_list)
-    return sync_list
-"""
 
 def sync(txt, sync_list, pth_src, pth_des, lbl):
     if txt: txt.delete(0.0, END)
@@ -151,7 +93,9 @@ def sync(txt, sync_list, pth_src, pth_des, lbl):
     for item in sync_list[0]:
         pth_to = os.path.join(pth_des, item)
         os.mkdir(pth_to)
-        if txt: txt.insert(END, 'Create Path => %s\n' % pth_to)
+        if txt:
+            txt.insert(END, 'Create Path => %s\n' % pth_to)
+            txt.see(END)
         count += 1
         lbl.config(text='%s/%s' % (count, tot))
 
@@ -168,7 +112,9 @@ def sync(txt, sync_list, pth_src, pth_des, lbl):
                     file.close()
                 break
             file_out.write(data)
-        if txt: txt.insert(END, 'Creath File ==> %s\n' % pth_to)
+        if txt:
+            txt.insert(END, 'Creath File ==> %s\n' % pth_to)
+            txt.see(END)
         count += 1
         lbl.config(text='%s/%s' % (count, tot))
 
@@ -181,99 +127,12 @@ def sync(txt, sync_list, pth_src, pth_des, lbl):
             data = src_file.read(10000000)
             if not data: break
             des_file.write(data)
-        if txt: txt.insert(END, 'Changed >>> %s\n' % pth_from)
+        if txt:
+            txt.insert(END, 'Changed >>> %s\n' % pth_from)
+            txt.see(END)
         count += 1
         lbl.config(text='%s/%s' % (count, tot))
-"""
-def listdir(pth):
-    result = []
-    def sub_list(pth, result):
-        for item in os.listdir(pth):
-            real_pth = os.path.join(pth, item)
-            if os.path.isdir(real_pth):
-                result.append(real_pth)
-                sub_list(real_pth, result)
-            elif os.path.isfile(real_pth):
-                result.append(real_pth)
-    sub_list(pth, result)
-    return result
 
-
-def compare(src_pth, des_pth):
-    if not os.path.exists(src_pth):
-        print('Source Path Not exist!')
-        return
-    elif not os.path.exists(des_pth):
-        print('Destination Path Not Exist!')
-        if input('Create Destination Path [%s]?' % des_pth)[0] in ['Y', 'y']:
-            os.mkdir(des_pth)
-        else:
-            return
-    src_pth = src_pth if src_pth[-1] == '/' else src_pth + '/'
-    des_pth = des_pth if des_pth[-1] == '/' else des_pth + '/'
-    src_pth_list = [x.replace(src_pth, '') for x in listdir(src_pth)]
-    des_pth_list = [x.replace(des_pth, '') for x in listdir(des_pth)]
-    file_new = []
-    dir_new = []
-    file_exist = []
-    for item in src_pth_list:
-        if item not in des_pth_list:
-            if os.path.isdir(os.path.join(src_pth, item)):
-                dir_new.append(item)
-            else:
-                file_new.append(item)
-        else:
-            file_exist.append(item)
-    return src_pth, dir_new, file_new, file_exist
-
-
-def sync_new_files(dir_list, file_list, src_pth, des_pth):
-    for item in dir_list:
-        pth_to = os.path.join(des_pth, item)
-        os.mkdir(pth_to)
-        print('Create Path => ', pth_to)
-    for item in file_list:
-        pth_to = os.path.join(des_pth, item)
-        pth_from = os.path.join(src_pth, item)
-        file_in = open(pth_from, 'rb')
-        file_out = open(pth_to, 'wb')
-        while True:
-            data = file_in.read(2048000)
-            if not data: break
-            file_out.write(data)
-        print('Creath File ==> ', pth_to)
-
-def sync_exist_files(file_list, src_pth, des_pth):
-    changed_list = []
-    for item in file_list:
-        pth_from = os.path.join(src_pth, item)
-        pth_to = os.path.join(des_pth, item)
-        if os.path.isdir(pth_from): continue
-        src_file = open(pth_from, 'rb')
-        des_file = open(pth_to, 'rb')
-        src_hash = hashlib.sha1()
-        des_hash = hashlib.sha1()
-        while True:
-            src_data = src_file.read(20000000)
-            des_data = des_file.read(20000000)
-            if not (src_data and des_data): break
-            src_hash.update(src_data)
-            des_hash.update(des_data)
-            if src_hash.hexdigest() != des_hash.hexdigest():
-                changed_list.append(item)
-                break
-
-    for item in changed_list:
-        pth_from = os.path.join(src_pth, item)
-        pth_to = os.path.join(des_pth, item)
-        src_file = open(pth_from, 'rb')
-        des_file = open(pth_to, 'wb')
-        while True:
-            data = src_file.read(10000000)
-            if not data: break
-            des_file.write(data)
-        print('Changed >>> ', pth_from)
-"""
 
 if __name__ == '__main__':
     test_pth = '/Users/joshuapu/Documents/Scripts'
--- SyncTool.py
+++ SyncTool.py
@@ -59,17 +59,28 @@ class SyncTool(Tk):
     def makeText(self):
         frm_txt = Frame(self)
         frm_txt.pack(side=TOP, fill=BOTH, expand=YES)
-        txt = Text(frm_txt)
+        scl = Scrollbar(frm_txt)
+        scl.pack(side=RIGHT, fill=Y)
+        txt = Text(frm_txt, wrap=NONE)
         txt.pack(side=TOP, fill=BOTH, expand=YES)
+        scl.config(command=txt.yview)
+        txt.config(yscrollcommand=scl.set)
         return txt
 
     def makeMenu(self):
         frm_menu = Frame(self)
         frm_menu.pack(side=BOTTOM, anchor=NW, fill=X)
-        for item in self.menu_list:
-            btn = Button(frm_menu, text=item['text'], height=1, width=8,
-                         command=item['command'])
-            btn.pack(side=item['side'])
+        btn_compare = Button(frm_menu, text='Compare', height=1, width=8, command=self.compare)
+        btn_compare.pack(side=LEFT)
+        self.btn_sync = Button(frm_menu, text='Sync', height=1, width=8, command=self.sync,
+                          state=DISABLED)
+        self.btn_sync.pack(side=LEFT)
+        btn_quit = Button(frm_menu, text='Quit', height=1, width=8, command=self.exit)
+        btn_quit.pack(side=RIGHT)
+
+        #   btn = Button(frm_menu, text=item['text'], height=1, width=8,
+        #                 command=item['command'])
+        #    btn.pack(side=item['side'])
 
 
 ###################################
@@ -80,11 +91,13 @@ class SyncTool(Tk):
         self.pth_src = askdirectory(initialdir='~', title='Select Source Folder')
         if self.pth_src:
             self.lbl_pth_src.config(text=self.pth_src)
+            self.btn_sync.config(state=DISABLED)
 
     def selectDest(self):
         self.pth_des = askdirectory(initialdir='~', title='Select Destination Folder')
         if self.pth_des:
             self.lbl_pth_des.config(text=self.pth_des)
+            self.btn_sync.config(state=DISABLED)
 
     def exit(self):
         if askyesno('Quit', 'Really to quit?'):
@@ -94,8 +107,7 @@ class SyncTool(Tk):
         self.stat_names = [('NewFiles:', 0, 0), ('NewFolders:', 0, 2),
                            ('DiffFiles:', 0, 4),
                            ('Total:', 1, 0), ('Progress:', 1, 2)]
-        self.menu_list = [{'text':'AutoSync', 'command':self.AutoSync, 'side':'left'},
-                          {'text':'Compare', 'command':self.compare, 'side':'left'},
+        self.menu_list = [{'text':'Compare', 'command':self.compare, 'side':'left'},
                           {'text':'Sync', 'command':self.sync, 'side':'left'},
                           {'text':'Quit', 'command':self.exit, 'side':'right'}]
 
@@ -114,114 +126,14 @@ class SyncTool(Tk):
 
         self.sync_list.append([])
         _thread.start_new_thread(SyncFunc.compare_exist,
-                                 (file_exist, self.pth_src, self.pth_des, self.sync_list[2], self.lbl_diff))
+                                 (file_exist, self.pth_src, self.pth_des, self.sync_list[2],
+                                  self.lbl_diff, self.btn_sync))
 
     def sync(self):
         _thread.start_new_thread(SyncFunc.sync,
                                  (self.txt, self.sync_list, self.pth_src, self.pth_des, self.lbl_prog))
 
-    def AutoSync(self):
-        pass
 
-
-"""
-###################################
-# 同步功能
-###################################
-
-    def listdir(self, pth):
-        result = []
-        def sub_list(pth, result):
-            for item in os.listdir(pth):
-                real_pth = os.path.join(pth, item)
-                if os.path.isdir(real_pth):
-                    result.append(real_pth)
-                    sub_list(real_pth, result)
-                elif os.path.isfile(real_pth):
-                    result.append(real_pth)
-        sub_list(pth, result)
-        return result
-
-    def compare(self, pth_src, pth_des):
-        file_new = []
-        dir_new = []
-        file_exist = []
-
-        if not (self.pth_src and os.path.exists(self.pth_src)):
-            print('Source Path Not exist!')
-            return
-        elif not (self.pth_des and os.path.exists(self.pth_des)):
-            print('Destination Path Not Exist!')
-            if askyesno('Create Destination Path [%s]?' % self.des_pth):
-                os.mkdir(self.des_pth)
-            else:
-                return
-
-        src_pth = self.pth_src if self.pth_src[-1] == '/' else self.pth_src + '/'
-        des_pth = self.pth_des if self.pth_des[-1] == '/' else self.pth_des + '/'
-        src_pth_list = [x.replace(src_pth, '') for x in self.listdir(src_pth)]
-        des_pth_list = [x.replace(des_pth, '') for x in self.listdir(des_pth)]
-
-        for item in src_pth_list:
-            if item not in des_pth_list:
-                if os.path.isdir(os.path.join(src_pth, item)):
-                    dir_new.append(item)
-                else:
-                    file_new.append(item)
-            else:
-                file_exist.append(item)
-
-        changed_list = []
-        for item in file_exist:
-            pth_from = os.path.join(self.pth_src, item)
-            pth_to = os.path.join(self.pth_des, item)
-            if os.path.isdir(pth_from): continue
-            src_file = open(pth_from, 'rb')
-            des_file = open(pth_to, 'rb')
-            src_hash = hashlib.sha1()
-            des_hash = hashlib.sha1()
-            while True:
-                src_data = src_file.read(20000000)
-                des_data = des_file.read(20000000)
-                if not (src_data and des_data): break
-                src_hash.update(src_data)
-                des_hash.update(des_data)
-                if src_hash.hexdigest() != des_hash.hexdigest():
-                    changed_list.append(item)
-                    break
-
-        self.sync_list = (dir_new, file_new, changed_list)
-
-    def sync(self):
-        self.txt.delete(0.0, END)
-
-        for item in self.sync_list[0]:
-            pth_to = os.path.join(self.pth_des, item)
-            os.mkdir(pth_to)
-            self.txt.insert(END, 'Create Path => %s\n' % pth_to)
-
-        for item in self.sync_list[1]:
-            pth_to = os.path.join(self.pth_des, item)
-            pth_from = os.path.join(self.pth_src, item)
-            file_in = open(pth_from, 'rb')
-            file_out = open(pth_to, 'wb')
-            while True:
-                data = file_in.read(2048000)
-                if not data: break
-                file_out.write(data)
-            self.txt.insert(END, 'Creath File ==> %s\n' % pth_to)
-
-        for item in self.sync_list[2]:
-            pth_from = os.path.join(self.pth_src, item)
-            pth_to = os.path.join(self.pth_des, item)
-            src_file = open(pth_from, 'rb')
-            des_file = open(pth_to, 'wb')
-            while True:
-                data = src_file.read(10000000)
-                if not data: break
-                des_file.write(data)
-            self.txt.insert(END, 'Changed >>> %s\n' % pth_from)
-"""
 
 if __name__ == '__main__':
     import sys
@@ -229,9 +141,13 @@ if __name__ == '__main__':
         def selectSrc(self):
             self.pth_src = r'G:\BackupTest' if sys.platform == 'win32' else r'/Users/joshuapu/Documents/Scripts'
             self.lbl_pth_src.config(text=self.pth_src)
+            self.btn_sync.config(state=DISABLED)
+
 
         def selectDest(self):
             self.pth_des = r'Y:\Backup' if sys.platform == 'win32' else r'/Users/joshuapu/Documents/Backup'
             self.lbl_pth_des.config(text=self.pth_des)
+            self.btn_sync.config(state=DISABLED)
+
     Test()
     mainloop()
